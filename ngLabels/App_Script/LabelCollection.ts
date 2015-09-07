1d028cf487817ea7007ff94cc6f683289f1eb2a8
Bill Wagner <bill@thebillwagner.com>
2015-09-07 18:09:52 -0400

    Controller works well
    Read only, and we can see information

---
+++ ngLabels/App_Script/LabelCollection.ts
@@ -0,0 +1,22 @@
+﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+
+module LabelApplication {
+    import ngr = ng.resource;
+
+    export class LabelCollection {
+        constructor(
+            private $scope: ng.IScope,
+            private service: LabelDataService) {
+            this.sequence = service.retrieveAllLabels();
+        }
+
+        public update(label: LabelApplication.Rest.Label) {
+            this.service.updateLabel(label);
+        }
+
+        sequence: ngr.IResourceArray<LabelApplication.Rest.Label>;
+    }
+    LabelEditor
+        .editorModule
+        .controller("labelCollection", ["$scope", "labelDataService", LabelCollection]);
+}
