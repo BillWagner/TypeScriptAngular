663791f844168c13ad2312e63f0923ad65ccca25
Bill Wagner <bill@thebillwagner.com>
2015-09-09 14:53:06 -0400

    Add the content editable typescript file

---
+++ ngLabels/App_Script/contenteditable.ts
@@ -0,0 +1,26 @@
+﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+
+module LabelApplication {
+    class ContentEditable implements ng.IDirective {
+        static factory(): ng.IDirectiveFactory {
+            return () => new ContentEditable();
+        }
+
+        restrict = 'A';
+        require = 'ngModel';
+        link = (scope: ng.IScope, element: ng.IRootElementService, attrs: any,
+            ngModel: ng.INgModelController) => {
+            function read() {
+                ngModel.$setViewValue(element.html());
+            }
+            ngModel.$render = function () {
+                element.html(ngModel.$viewValue || "");
+            };
+            element.bind("blur keyup change", function () {
+                scope.$apply(read);
+            });
+
+        };
+    }
+    LabelEditor.editorModule.directive('contenteditable', ContentEditable.factory());
+} 
